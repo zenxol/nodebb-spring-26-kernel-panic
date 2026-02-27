18c806cb5863af2582a9df151286b146d4275975
anthony-tom <anthonytom3811@gmail.com>
2026-02-27 16:22:45 -0500

    Fix: remove unused getLevenshteinDistance function

--- public/src/modules/search.js
+++ public/src/modules/search.js
@@ -314,23 +314,6 @@ define('search', [
 		}
 	};
 
-	function getLevenshteinDistance(a, b) {
-		if (a.length === 0) return b.length;
-		if (b.length === 0) return a.length;
-		const m = a.length;
-		const n = b.length;
-		const d = Array(m + 1).fill(null).map(() => Array(n + 1).fill(0));
-		for (let i = 0; i <= m; i++) d[i][0] = i;
-		for (let j = 0; j <= n; j++) d[0][j] = j;
-		for (let i = 1; i <= m; i++) {
-			for (let j = 1; j <= n; j++) {
-				const cost = a[i - 1] === b[j - 1] ? 0 : 1;
-				d[i][j] = Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost);
-			}
-		}
-		return d[m][n];
-	}
-
 	function maxFuzzyEdits(len) {
 		if (len <= 5) return 1;
 		if (len <= 9) return 2;
